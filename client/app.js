c4b34b88ce59289718c5252ed549ae62f46ec604
joshua park <joshuapark@joshuas-MacBook-Pro.local>
2018-04-18 12:32:28 -0400

    added Route component to app Component

--- client/app.js
+++ client/app.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import { Navbar, AllProducts } from './components';
+import Routes from './routes'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <AllProducts />
+      <Routes />
     </div>
   );
 };
