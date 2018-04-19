bf5893b3ae06054bb17890caf5a7125e8d67d9bb
Marshall Zobel <dmzobel@gmail.com>
2018-04-19 18:05:49 -0400

    add: get route for the cart

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 module.exports = router;
 
+router.get('/', (req, res, next) => {
+  res.status(200).json(req.session.cart);
+});
+
 // POST /cart
 router.post('/', (req, res, next) => {
   const productToAdd = req.body;
