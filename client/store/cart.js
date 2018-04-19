bf7af134e5f36ff57866e05917bc8169a52d1736
Casey Chan <caseygocoding@gmail.com>
2018-04-19 18:59:19 -0400

    render checkout page and update the total price

--- client/store/cart.js
+++ client/store/cart.js
@@ -2,14 +2,20 @@ import axios from 'axios';
 import history from '../history';
 
 const POST_SINGLE_CART = 'POST_SINGLE_CART';
+const GET_SINGLE_CART = 'GET_SINGLE_CART';
 
 const postSingleCart = singleProduct => ({
   type: POST_SINGLE_CART,
   singleProduct,
 });
 
-export function postCart(product) {
-  return function(dispatch) {
+const getSingleCart = singleCart => ({
+  type: GET_SINGLE_CART,
+  singleCart,
+});
+
+export function postCart (product) {
+  return function (dispatch) {
     return axios
       .post(`/api/cart`, product)
       .then(res => dispatch(postSingleCart(res.data)))
@@ -17,13 +23,25 @@ export function postCart(product) {
   };
 }
 
-export default function cartReducer(state = {}, action) {
+export function getCart () {
+  return function (dispatch) {
+    return axios
+      .get(`/api/cart`)
+      .then(res => dispatch(getSingleCart(res.data)))
+      .catch(err => console.error(err));
+  };
+}
+
+export default function cartReducer (state = {}, action) {
   switch (action.type) {
     case POST_SINGLE_CART:
       return {
         ...state,
         [action.singleProduct.id]: action.singleProduct,
       };
+
+    case GET_SINGLE_CART:
+      return action.singleCart;
 
     default:
       return state;
