7292be0f771ee2e579b246f8f22b22a103466619
Marshall Zobel <dmzobel@gmail.com>
2018-04-24 10:46:28 -0400

    add: gatekeeper middleware to user route

--- server/api/users.js
+++ server/api/users.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
-const {User} = require('../db/models');
+const { User } = require('../db/models');
 module.exports = router;
 
 // AP: TODO- put gatekeeperMiddleware in a `utils` module to be used here and elsewhere
-//     should be on most of your routes on the backend
+// should be on most of your routes on the backend
 
-router.get('/', (req, res, next) => {
+// this is an example of a gatekeeper middleware func
+const isAdmin = (req, res, next) => {
+  if (req.user.isAdmin) next();
+  else res.sendStatus(401);
+};
+
+// router.<method> takes an unlimited number of middleware funcs
+router.get('/', isAdmin, (req, res, next) => {
   User.findAll({
     // explicitly select only the id and email fields - even though
     // users' passwords are encrypted, it won't help if we just
